a37e218cf6d9951c6b075dedbbfd09f0ee50c26f
HieuTranTrung <hieu.trantrung1204@gmail.com>
2023-10-07 13:49:32 +0700

    Expose parameter color and background color

--- lib/picker_view.dart
+++ lib/picker_view.dart
@@ -2,11 +2,12 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 typedef PickerRowCallBack = int Function(int section);
-typedef PickerItemBuilder = Widget Function(int section,int row);
+typedef PickerItemBuilder = Widget Function(int section, int row);
 typedef PickerVoidCallBack = void Function(int section, int row);
 
 class PickerView extends StatefulWidget {
-  
+  final Color? color;
+  final Color? backgroundColor;
   final PickerRowCallBack numberofRowsAtSection;
   final PickerItemBuilder itemBuilder;
   final PickerVoidCallBack? onSelectRowChanged;
@@ -14,6 +15,8 @@ class PickerView extends StatefulWidget {
   final PickerController controller;
 
   PickerView({
+    this.color,
+    this.backgroundColor,
     required this.numberofRowsAtSection,
     required this.itemBuilder,
     required this.controller,
@@ -28,14 +31,13 @@ class PickerView extends StatefulWidget {
 }
 
 class PickerViewState extends State<PickerView> {
-  
   late PickerController _controller;
-  
+
   @override
   void initState() {
     super.initState();
   }
-  
+
   @override
   void didUpdateWidget(PickerView oldWidget) {
     super.didUpdateWidget(oldWidget);
@@ -43,7 +45,7 @@ class PickerViewState extends State<PickerView> {
       _controller = widget.controller;
     }
   }
-  
+
   @override
   void didChangeDependencies() {
     _controller = widget.controller;
@@ -53,75 +55,70 @@ class PickerViewState extends State<PickerView> {
   @override
   Widget build(BuildContext context) {
     return Material(
-        child: Container(
-          color: Colors.white,
-          child: Row(
-            children: _buildPickers()
-          )
-        ),
+      child:
+          Container(color: widget.color?? Colors.white, child: Row(children: _buildPickers())),
     );
   }
-  
+
   List<Widget> _buildPickers() {
     List<Widget> children = [];
-    
+
     for (int section = 0; section < _controller.count; section++) {
-      children.add(
-        Expanded (flex: 1, child: _buildPickerItem (section: section))
-      );
+      children
+          .add(Expanded(flex: 1, child: _buildPickerItem(section: section)));
     }
-    
+
     return children;
   }
 
   Widget _buildPickerItem({required int section}) {
-  
-    FixedExtentScrollController scrollController = _controller.scrollControllers[section];
-    
+    FixedExtentScrollController scrollController =
+        _controller.scrollControllers[section];
+
     return Container(
       child: CupertinoPicker.builder(
-        backgroundColor: Colors.white,
+        backgroundColor: widget.backgroundColor ?? Colors.white,
         scrollController: scrollController,
         diameterRatio: 1,
         itemExtent: widget.itemExtent ?? 40,
         childCount: widget.numberofRowsAtSection(section),
         onSelectedItemChanged: (row) {
-          if(widget.onSelectRowChanged != null) {
+          if (widget.onSelectRowChanged != null) {
             widget.onSelectRowChanged!(section, row);
           }
         },
-        itemBuilder: (context,row) {
+        itemBuilder: (context, row) {
           return Container(
-            alignment: Alignment.center,
-            child: widget.itemBuilder(section,row)
-          );
+              alignment: Alignment.center,
+              child: widget.itemBuilder(section, row));
         },
       ),
     );
   }
 }
 
-class PickerController  {
-  
+class PickerController {
   final int count;
   final List<FixedExtentScrollController> scrollControllers;
 
-  PickerController({required this.count, List<int>? selectedItems}) :  scrollControllers = [] {
-    for (int i = 0; i< count; i++) {
+  PickerController({required this.count, List<int>? selectedItems})
+      : scrollControllers = [] {
+    for (int i = 0; i < count; i++) {
       if (selectedItems != null && i < selectedItems.length) {
-        scrollControllers.add(FixedExtentScrollController(initialItem: selectedItems[i]));
+        scrollControllers
+            .add(FixedExtentScrollController(initialItem: selectedItems[i]));
       } else {
         scrollControllers.add(FixedExtentScrollController());
       }
     }
   }
-  
+
   void dispose() {
-    scrollControllers.forEach((item){
+    scrollControllers.forEach((item) {
       item.dispose();
     });
   }
-  
+
   int? selectedRowAt({required int section}) {
     try {
       FixedExtentScrollController scrollController = scrollControllers[section];
@@ -130,7 +127,7 @@ class PickerController  {
       } else {
         return null;
       }
-    } catch(err) {
+    } catch (err) {
       return null;
     }
   }
@@ -140,27 +137,26 @@ class PickerController  {
       if (scrollControllers.length <= atSection) {
         return;
       }
-      FixedExtentScrollController scrollController = scrollControllers[atSection];
+      FixedExtentScrollController scrollController =
+          scrollControllers[atSection];
       scrollController.jumpToItem(row);
-    } catch(err) {
-    
-    }
+    } catch (err) {}
   }
 
   Future<void> animateToRow(
-      int row, {
-      required int atSection,
-      Duration duration = const Duration(milliseconds: 300),
-      Curve curve = Curves.easeInOut,
+    int row, {
+    required int atSection,
+    Duration duration = const Duration(milliseconds: 300),
+    Curve curve = Curves.easeInOut,
   }) async {
     try {
       if (scrollControllers.length <= atSection) {
         return;
       }
-      FixedExtentScrollController scrollController = scrollControllers[atSection];
-      await scrollController.animateToItem(row, duration: duration, curve: curve);
-    } catch(err) {
-    
-    }
+      FixedExtentScrollController scrollController =
+          scrollControllers[atSection];
+      await scrollController.animateToItem(row,
+          duration: duration, curve: curve);
+    } catch (err) {}
   }
 }
